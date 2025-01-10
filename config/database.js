9d554e4bc23bebb49b70a74bbe3926753aa4c628
Soareslili <lidianesantossoares2@gmail.com>
2025-01-10 16:43:27 -0300

    atualizando o projeto com nossas extensões e criando o banco de dados

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    hots: 'localhost',
+    port: '5432',
+    username: 'postgres',
+    password: 'postegres',
+    database: 'devburger',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
---
+++ models/User.js
@@ -0,0 +1,18 @@
+import  Sequelize, {Model} from "sequelize";
+
+class User extends Model {
+    static init(sequelize){
+        super.init({
+            name: Sequelize.STRING,
+            email: Sequelize.STRING,
+            password_hash: Sequelize.STRING,
+            adnin: Sequelize.BOOLEAN,
+        },
+        {
+            sequelize,
+        },  
+    );
+    }
+}
+
+export default User;
